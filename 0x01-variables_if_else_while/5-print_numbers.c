fe92fe2d1331cc0483b90155c48c1c61abce4a25
josephorokpo <josephorokpo@gmail.com>
2023-06-16 14:58:36 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main – Entry point
  *
- * Description: prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Description: Prints all single digit numbers of base 10 starting from 0, followed by a new line.
  *
  * Return: Always 0 (Success)
  */

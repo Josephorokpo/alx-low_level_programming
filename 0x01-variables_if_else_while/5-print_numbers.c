f5215050e40cb16b891882491261eea3d33bd39f
josephorokpo <josephorokpo@gmail.com>
2023-06-16 14:29:58 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main – Entry point
  *
- * Description: Prints numbers from 0 to 9.
+ * Description: Prints numbers from 0 to 9, followed by a new line.
  *
  * Return: Always 0 (Success)
  */

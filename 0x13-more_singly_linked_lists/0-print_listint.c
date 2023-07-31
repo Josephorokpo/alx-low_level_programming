87dbfb35189a2779d54c571be33387ffe029bb50
josephorokpo <josephorokpo@gmail.com>
2023-07-31 06:14:41 -0700

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - Prints all elements of the linked list.
+ *
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of elements printed.
+ */
+size_t print_listint(const struct listint_t *h)
+{
+	size_t count = 0;
+	const struct listint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d ", current->data);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}

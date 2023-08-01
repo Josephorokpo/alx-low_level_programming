813ac7ea6c7c1f932d56d7c97c01df8521d3e4d4
josephorokpo <josephorokpo@gmail.com>
2023-08-01 13:55:20 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
+ * print_listint_safe - Prints listint_t linked list, avoids infinite loops.
  * @head: A pointer to the head of the listint_t list.
  *
  * Return: The number of nodes in the list.
@@ -12,20 +12,24 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t num_nodes = 0;
 	const listint_t *current = head;
+	const listint_t *temp;
 
 	while (current)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
-
 		num_nodes++;
 
 		if (current <= current->next)
 		{
+			/* Print the node that causes the cycle */
 			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
 			exit(98);
 		}
 
+		/* Mark the current node as visited by making it point back to itself */
+		temp = current;
 		current = current->next;
+		temp->next = (void *)temp;
 	}
 
 	return (num_nodes);

f4d737c05c36b8af41fab051ac75a826a6bcdf65
josephorokpo <josephorokpo@gmail.com>
2023-08-01 13:14:08 -0700

    11. Reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,12 +4,6 @@
  * reverse_listint - Reverses a linked list in-place.
  * @head: Pointer to the first node of the list.
  *
- * Description:
- *   This function takes a pointer to the head of a linked list and
- *   reverses the order of the nodes in the list. The reversal is done
- *   in-place, meaning no new nodes are created; the function modifies
- *   the pointers of the existing nodes to achieve the reversal.
- *
  * Return: Pointer to the new head of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)

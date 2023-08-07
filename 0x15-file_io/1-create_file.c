492f6b752a3bf8e85b591757d7bedf1281f2231d
josephorokpo <josephorokpo@gmail.com>
2023-08-07 06:50:58 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int fd;
+	size_t len = 0;
 	ssize_t bytes_written;
 
 	if (filename == NULL)
@@ -24,16 +25,13 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
 
 	bytes_written = write(fd, text_content, len);
-
 	close(fd);
 
 	if (bytes_written == -1 || (size_t)bytes_written != len)
-
 		return (-1);
 
 	return (1);

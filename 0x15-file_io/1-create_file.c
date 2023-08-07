a2820640f36742a6f7be3d44a55ee37c0243a123
josephorokpo <josephorokpo@gmail.com>
2023-08-07 06:42:12 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file and writes content to it.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: On success, 1.
+ *         On failure, -1.
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	bytes_written = write(fd, text_content, len);
+
+	close(fd);
+
+	if (bytes_written == -1 || (size_t)bytes_written != len)
+
+		return (-1);
+
+	return (1);
+}

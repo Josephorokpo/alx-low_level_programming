3110ec4f942396cd22f8a0ef2a30f187c041e735
josephorokpo <josephorokpo>
2023-09-04 05:23:30 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with specified permissions
- * and writes text to it.
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file (can be NULL).
+ * create_file - Creates a new file with optional content.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to the text content to write to
+ * the file (can be NULL).
  *
- * Return: 1 on success, -1 on failure.
+ * Return: On success, returns 1. On failure, returns -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = -1; /* File descriptor */
-	ssize_t bytes_written = 0; /* Bytes written to the file */
+	int fd = 0;
+	int w = 0;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
 	close(fd);
+
 	return (1);
 }

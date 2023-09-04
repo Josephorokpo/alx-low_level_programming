e5340e56ca2470e7a0cbabb5ad128a84abb61ea8
josephorokpo <josephorokpo>
2023-09-04 04:53:35 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,43 @@
-main.h
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - Reads and prints a text file to the standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed.
- */
+* read_textfile - Read a text file and print its content to STDOUT.
+* @filename: The name of the text file to be read.
+* @letters: The number of letters to read and print.
+*
+* Return: The actual number of bytes read and printed, 0 on failure.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t bytes_read, bytes_written;
-	FILE *file;
+	ssize_t file_descriptor;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
 
-	if (filename == NULL)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
-	file = fopen(filename, "r");
-	if (file == NULL)
-		return (0);
-
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		fclose(file);
+		close(file_descriptor);
 		return (0);
 	}
 
-	bytes_read = fread(buffer, sizeof(char), letters, file);
+	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
-		fclose(file);
 		free(buffer);
+		close(file_descriptor);
 		return (0);
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
 
-	fclose(file);
 	free(buffer);
+	close(file_descriptor);
 
 	return (bytes_written);
 }

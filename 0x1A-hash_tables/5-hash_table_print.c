07a223c310d785241b365f5abd2e1c1d1574a8f4
josephorokpo <josephorokpo@gmail.com>
2023-11-02 04:53:59 -0700

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table to print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, first = 1;
+	hash_node_t *current_node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			current_node = current_node->next;
+			first = 0;
+		}
+	}
+	printf("}\n");
+}

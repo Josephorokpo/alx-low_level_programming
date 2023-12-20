bbfe3291bd602c629cf36a4208bbf6ac281ed9f8
josephorokpo <josephorokpo@gmail.com>
2023-12-20 07:07:55 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
+"""Defines a function to calculate the perimeter of an island."""
 
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in the grid.
+    """Calculate the perimeter of an island.
 
-    Parameters:
-    - grid (list of list of integers): Represents the island, where 0 is water and 1 is land.
+    The function takes a 2D grid as input, where 0 represents water and 1 represents land.
+    It traverses the grid to count the perimeter of the island based on the specified rules.
+
+    Args:
+        grid (list of lists): A 2D list of integers representing an island.
 
     Returns:
-    - int: The perimeter of the island.
-
-    Constraints:
-    - Grid is a list of list of integers.
-    - 0 represents a water zone, and 1 represents a land zone.
-    - One cell is a square with side length 1.
-    - Grid cells are connected horizontally/vertically (not diagonally).
-    - Grid is rectangular, and width and height don’t exceed 100.
-    - Grid is completely surrounded by water, and there is one island (or nothing).
-    - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+        int: The perimeter of the island defined in the input grid.
     """
-    perimeter = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to the perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
 
-                # Check adjacent cells and subtract 1 for each neighboring land cell
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
-                if i < len(grid) - 1 and grid[i + 1][j] == 1:
-                    perimeter -= 1
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
-                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 1
-
-    return perimeter
+    return size * 4 - edges * 2

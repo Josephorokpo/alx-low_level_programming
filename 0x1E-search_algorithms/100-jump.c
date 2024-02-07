e40cb0f818be92ed8b666b9683cd201761073b4e
josephorokpo <josephorokpo@gmail.com>
2024-02-07 03:07:15 -0800

    100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Searches for a value in a sorted array using Jump search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where value is located, or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step = sqrt(size), prev = 0, i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (array[min(step, size) - 1] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev = step;
+		step += sqrt(size);
+		if (prev >= size)
+			return (-1);
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
+
+	for (i = prev; i < min(step, size); i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
+
+/**
+ * min - Returns the minimum of two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The minimum of a and b
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a < b ? a : b);
+}
